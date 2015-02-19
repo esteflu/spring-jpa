b15dd7939f8e5c99c34146f2b390cddd86bad252
stefanlundberg <lundberg.stefan@gmail.com>
2015-02-19 20:01:19 +0100

    Removed repo operation

--- src/main/java/example/repository/PersonRepository.java
+++ src/main/java/example/repository/PersonRepository.java
@@ -1,12 +1,8 @@
 package example.repository;
 
 import example.domain.Person;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
-
-    Page<Person> findAll(Pageable pageable);
 }
